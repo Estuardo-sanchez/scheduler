e72b5dce09cc9034281def91113deee6ae54e06f
Estuardo <estuardo_101@hotmail.com>
2022-08-05 20:33:20 -0400

    add custom hook & edit appoinment component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,8 +3,8 @@ import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
-  const [student, setStudent] = useState(props.student || "");
-  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  let [student, setStudent] = useState(props.student || "");
+  let [interviewer, setInterviewer] = useState(props.interviewer || null);
 
   const reset = () => {
     setStudent= ("")
