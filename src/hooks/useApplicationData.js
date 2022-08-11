c055702c49af4c2580de509f4b63808ef2ddd538
Estuardo <estuardo_101@hotmail.com>
2022-08-11 16:43:50 -0400

    update spots

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -26,7 +26,8 @@ export default function useApplicationData() {
       console.log(res);
       setState({
         ...state,
-        appointments
+        appointments,
+        days: updateSpots(state, appointments)
       });
     })
   };
@@ -63,7 +64,10 @@ export default function useApplicationData() {
 
     return axios.delete(`http://localhost:8001/api/appointments/${id}`)
       .then(res => {
-        setState({ ...state, appointments, days })
+        setState({
+          ...state,
+          appointments,
+          days: updateSpots(state, appointments) })
         return res
       })
   }
@@ -80,6 +84,21 @@ export default function useApplicationData() {
     setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
   });
  })
+
+ function updateSpots(state, appointments) {
+  return state.days.map((elem) => {
+    if (elem.name === state.day) {
+      return {
+        ...elem,
+        spots: elem.appointments
+          .map((appointment) => appointments[appointment])
+          .filter(({ interview }) => !interview).length
+      };
+    }
+
+    return elem;
+  });
+}
 
  return { state, setDay, bookInterview, cancelInterview };
 }
