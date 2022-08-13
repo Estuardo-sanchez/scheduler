d215cfdb1f7598dfd0f7c6966dea842a9f688bd3
Estuardo <estuardo_101@hotmail.com>
2022-08-13 19:26:16 -0400

    mocking modules

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -81,9 +81,10 @@ export default function useApplicationData() {
     axios.get('http://localhost:8001/api/appointments'),
     axios.get('http://localhost:8001/api/interviewers')
   ]).then((all) => {
+    console.log({all})
     setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
   });
- })
+ }, [])
 
  function updateSpots(state, appointments) {
   return state.days.map((elem) => {
